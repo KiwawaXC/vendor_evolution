TheParasiteProject/vendor_aosp

vendor: Include makefile with lmkd definitions
Change-Id: Ie950c753f786ce8cd2a3d2ccd78aa7d4670c3e3d

config/common.mk
@@ -162,6 +162,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Inherit art options
 include vendor/aosp/config/art.mk
 
+# Inherit LMKD options
+include vendor/aosp/config/lmkd.mk
+
 # Fonts
 $(call inherit-product, vendor/aosp/config/fonts.mk)
 

KiwawaXC/vendor_evolution

Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

config/common.mk
@@ -283,6 +283,9 @@ $(call inherit-product, vendor/bcr/bcr.mk)
 # Inherit from art config
 include vendor/evolution/config/art.mk
 
+# Inherit from LMKD config
+include vendor/evolution/config/lmkd.mk
+
 # Conditionally inherit firmware
 TARGET_SHIPS_FIRMWARE ?= false
 ifeq ($(TARGET_SHIPS_FIRMWARE),true)